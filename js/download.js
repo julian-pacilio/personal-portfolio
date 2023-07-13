031c569caf421beb8ae2a9c955ed48d8bf220fb6
julian-pacilio <julian.pacilio.info@gmail.com>
2023-07-13 14:33:04 -0300

    Route fixed

--- js/download.js
+++ js/download.js
@@ -2,7 +2,7 @@
 
 const downloadResume = document.querySelector('.button');
 const downloadFile = () => {
-            downloadResume.href = 'http://localhost:63342/assets/Resume_Pacilio_Julian_April_2023.pdf';
+            downloadResume.href = 'https://pacilio-julian.vercel.app/assets/Resume_Pacilio_Julian_April_2023.pdf';
             downloadResume.target = '_blank';
             downloadResume.download = 'Resume - Pacilio Julian - April 2023';
 };
